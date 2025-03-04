5b6471f9c466f349d635ce70119848685b678f9b
obiasmarkjhon_bsit@plmun.edu.ph <obiasmarkjhon_bsit@plmun.edu.ph>
2025-03-04 17:33:42 +0800

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,9 @@
+# MARKJOHN OBIAS
+# Week 05 - Working with Functions
+# Laboratory # 16 - Guided Coding Exercise: Basic Function Definition and Calling
+
+def greet():
+    """Prints a greeting message."""  # Docstring
+    print("Hello, Python Students!")  # Function body
+
+greet()  # Calling the function
