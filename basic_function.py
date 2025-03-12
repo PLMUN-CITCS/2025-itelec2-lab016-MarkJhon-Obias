6f0dae0429291923cde0ee470cc8061717f3f6f4
MarkJhon-Obias <obiasmarkjhon_bsit@plmun.edu.ph>
2025-03-12 11:49:59 +0800

    Delete basic_function.py

--- basic_function.py
+++
@@ -1,9 +0,0 @@
-# MARKJOHN OBIAS
-# Week 05 - Working with Functions
-# Laboratory # 16 - Guided Coding Exercise: Basic Function Definition and Calling
-
-def greet():
-    """Prints a greeting message."""  # Docstring
-    print("Hello, Python Students!")  # Function body
-
-greet()  # Calling the function
